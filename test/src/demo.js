a212b4b9a5dd14e10e7a652379e3a0fe7376430e
Matthew Green <matt@Matthews-MacBook-Pro.local>
2015-10-12 20:37:36 +0100

    More setup

--- test/src/demo.js
+++ test/src/demo.js
@@ -1,12 +1,7 @@
-'use strict';
 var assert = require('chai').assert;
 
-(function() {
-    
-    describe('demo', function() {
-        it('should pass', function() {
-            assert.isTrue(true);
-        });
+suite('demo', function() {
+    test('should pass', function() {
+        assert.isTrue(true);
     });
-
-})();
+});
